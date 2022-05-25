fda796d5e0218d5d81304d2f07f0e7b27f6a7f0b
Prakash Panday <prakashpanday757@gmail.com>
2022-05-25 14:26:57 +0545

    login usercheck function

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,48 +10,55 @@ class LoginController extends Controller
 {
     //for login
     public function userlogin(Request $request)
-    {
+    
         // Check validation
         // $this->validate($request, [
         //     'mobile_no' => 'required|regex:/[0-9]{10}/|digits:10',
         // ]);
-
-        // Get user record
-        // dd($request->all());
-        $user = User::where('username', $request->username)->first();
-        // dd($user);
-        // Check Condition Mobile No. Found or Not
-        if ($request->get('username') != $user->username) {
-            // \Session::put('errors', 'username does not match in our system..!!');
+        {
+       
+            $user = User::where('username', $request->username)->first();
+            // dd($user);
+            // Check Condition Mobile No. Found or Not
+            if(!is_null($user)){
+            if ($request->get('username') != $user->username) {
+                // \Session::put('errors', 'username does not match in our system..!!');
+                return response([
+                    'message' => ['Username is invalid'],
+                ]);
+            }
+        }
+        else{
             return response([
                 'message' => ['Username is invalid'],
             ]);
         }
-        // return $user->password;
-        $pass = $request->get('password');
-        $salt = "abcde";
-        $sha1 = sha1($salt . $pass);
-        $password = 'sha1$' . $salt . '$' . $sha1;
-        // return $password;
-        if ($password != $user->password) {
-            // \Session::put('errors', 'Your password doesnot match..!!');
-            return response([
-                'message' => ['password did not match'],
-            ]);
+        
+            // return $user->password;
+            $pass = $request->get('password');
+            $salt = "abcde";
+            $sha1 = sha1($salt . $pass);
+            $password = 'sha1$' . $salt . '$' . $sha1;
+            // return $password;
+            if ($password != $user->password) {
+                // \Session::put('errors', 'Your password doesnot match..!!');
+                return response([
+                    'message' => ['password did not match'],
+                ]);
+            }
+            $token = $user->createToken($user->username)->plainTextToken;
+    
+            // Set Auth Details
+            // dd($user);
+            Auth::login($user);
+    
+            // Redirect home page
+            $response = [
+                'user' => $user,
+                'token' => $token,
+            ];
+            return response($response, 200);
         }
-        $token = $user->createToken($user->username)->plainTextToken;
-
-        // Set Auth Details
-        // dd($user);
-        Auth::login($user);
-
-        // Redirect home page
-        $response = [
-            'user' => $user,
-            'token' => $token,
-        ];
-        return response($response, 200);
-    }
     public function userlogout(Request $request)
     {
         $request->user()->currentAccessToken()->delete();
