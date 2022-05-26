9c2779c0b7a51bc3355e83655c3e2c2c82658d8b
Prakash Panday <prakashpanday757@gmail.com>
2022-05-26 12:58:46 +0545

    new functionality developed for filter and bug fix on employee

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -9,6 +9,30 @@ class EmployeeController extends Controller
 {
     public function getemployee(){
         $employee = DB::table('iclock')->join('departments','iclock.DeptID','=','departments.DeptId')->rightJoin('userinfo','departments.DeptId','=', 'userinfo.defaultdeptid')->select('iclock.SN','departments.DeptName','userinfo.name','userinfo.Card','userinfo.Gender','userinfo.Birthday','userinfo.FPHONE','userinfo.pager','userinfo.minzu','userinfo.title')->get();
-        return response($employee, 'Employee data retrieved Sucessfully');
+        return response($employee, 200);
     }
+    public function filteroption(Request $request){
+         if(isset($request->birthday) && isset($request->departmentname) && isset($request->employee)){
+            $filter = DB::table('iclock')->join('departments','iclock.DeptID','=','departments.DeptId')->rightJoin('userinfo','departments.DeptId','=', 'userinfo.defaultdeptid')->where('userinfo.Birthday',$request->birthday)->where('departments.DeptName',$request->departmentname)->where('userinfo.name',$request->employee)->select('iclock.SN','departments.DeptName','userinfo.name','userinfo.Card','userinfo.Gender','userinfo.Birthday','userinfo.FPHONE','userinfo.pager','userinfo.minzu','userinfo.title')->get();
+            
+        }
+         elseif(isset($request->birthday) && isset($request->departmentname)){
+            $filter = DB::table('iclock')->join('departments','iclock.DeptID','=','departments.DeptId')->rightJoin('userinfo','departments.DeptId','=', 'userinfo.defaultdeptid')->where('userinfo.Birthday',$request->birthday)->where('departments.DeptName',$request->departmentname)->select('iclock.SN','departments.DeptName','userinfo.name','userinfo.Card','userinfo.Gender','userinfo.Birthday','userinfo.FPHONE','userinfo.pager','userinfo.minzu','userinfo.title')->get();
+         }
+         elseif(isset($request->birthday) && isset($request->employee)){
+            $filter = DB::table('iclock')->join('departments','iclock.DeptID','=','departments.DeptId')->rightJoin('userinfo','departments.DeptId','=', 'userinfo.defaultdeptid')->where('userinfo.Birthday',$request->birthday)->where('userinfo.name',$request->employee)->select('iclock.SN','departments.DeptName','userinfo.name','userinfo.Card','userinfo.Gender','userinfo.Birthday','userinfo.FPHONE','userinfo.pager','userinfo.minzu','userinfo.title')->get();
+         }
+         elseif(isset($request->departmentname) && isset($request->employee)){
+            $filter = DB::table('iclock')->join('departments','iclock.DeptID','=','departments.DeptId')->rightJoin('userinfo','departments.DeptId','=', 'userinfo.defaultdeptid')->where('departments.DeptName',$request->departmentname)->where('userinfo.name',$request->employee)->select('iclock.SN','departments.DeptName','userinfo.name','userinfo.Card','userinfo.Gender','userinfo.Birthday','userinfo.FPHONE','userinfo.pager','userinfo.minzu','userinfo.title')->get();
+         }
+         elseif(isset($request->birthday)){
+            $filter = DB::table('iclock')->join('departments','iclock.DeptID','=','departments.DeptId')->rightJoin('userinfo','departments.DeptId','=', 'userinfo.defaultdeptid')->where('userinfo.Birthday',$request->birthday)->select('iclock.SN','departments.DeptName','userinfo.name','userinfo.Card','userinfo.Gender','userinfo.Birthday','userinfo.FPHONE','userinfo.pager','userinfo.minzu','userinfo.title')->get();
+         }
+         elseif(isset($request->department)){
+            $filter = DB::table('iclock')->join('departments','iclock.DeptID','=','departments.DeptId')->rightJoin('userinfo','departments.DeptId','=', 'userinfo.defaultdeptid')->where('departments.DeptName',$request->departmentname)->select('iclock.SN','departments.DeptName','userinfo.name','userinfo.Card','userinfo.Gender','userinfo.Birthday','userinfo.FPHONE','userinfo.pager','userinfo.minzu','userinfo.title')->get();
+         }
+         else{
+            $filter = DB::table('iclock')->join('departments','iclock.DeptID','=','departments.DeptId')->rightJoin('userinfo','departments.DeptId','=', 'userinfo.defaultdeptid')->where('userinfo.name',$request->employee)->select('iclock.SN','departments.DeptName','userinfo.name','userinfo.Card','userinfo.Gender','userinfo.Birthday','userinfo.FPHONE','userinfo.pager','userinfo.minzu','userinfo.title')->get();
+         }
+         return response($filter, 200);    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-    protected $table = 'users';
+    protected $table = 'auth_user';
     public $timestamps = false;
     /**
      * The attributes that are mass assignable.
